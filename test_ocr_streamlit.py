1a17c147bfc23b9763c1aed8c7536ace85bb406f
Maria Elisa Rocha <mariaelisarochabouzo@gmail.com>
2025-07-09 09:29:06 +0200

    Add files via upload

--- test_ocr_streamlit.py
+++ test_ocr_streamlit.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import os
 from pdf2image import convert_from_bytes
-
-# Añadir poppler al PATH dentro del script
-os.environ["PATH"] += os.pathsep + r"C:\Users\Maria\Documents\poppler-24.08.0\Library\bin"
 
 st.title("Test OCR con pdf2image y Poppler")
 
@@ -13,7 +9,7 @@ if uploaded_file is not None:
     pdf_bytes = uploaded_file.read()
 
     try:
-        images = convert_from_bytes(pdf_bytes)  # Ya no pasamos poppler_path
+        images = convert_from_bytes(pdf_bytes, poppler_path=r"C:\Users\Maria\Documents\poppler-24.08.0\Library\bin")
         st.success(f"Se generaron {len(images)} imágenes del PDF")
         for i, img in enumerate(images):
             st.image(img, caption=f"Página {i+1}")
